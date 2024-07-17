8a196d8f33f760facec9f95270dfb39ac02222e3
johurul islam <89250009+johurul-abir@users.noreply.github.com>
2024-07-17 21:53:48 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://studyhipe.vercel.app",
     credentials: true,
   })
 );
